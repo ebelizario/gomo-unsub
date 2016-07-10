d1a51ccf42d968a7440f7e23fa8306987a23ad42
efren <efren.belizario@gmail.com>
2016-07-10 15:34:00 -0400

    Align config file contents

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@
  */
 define('__APP_ROOT__', dirname(dirname(__FILE__)));
 $config = array(
-    'url' => 'http://driscollhp.gomocampaign.com/api.php',
-    'username' => 'driscoll_admin',
-    'password' => 'thyxcAjk518',
-    'logpath' => __APP_ROOT__ . '/unsub_log',
-    'logsuffix' => '_unsubbed.log'
+    'url'         => 'http://driscollhp.gomocampaign.com/api.php',
+    'username'    => 'driscoll_admin',
+    'password'    => 'thyxcAjk518',
+    'logpath'     => __APP_ROOT__ . '/unsub_log',
+    'logsuffix'   => '_unsubbed.log'
 );
